5bc072f4fa633786b183e09f37e17c907ed1a442
LyubomyrKl <lyubomyrklymyuk2001@gmail.com>
2023-07-07 22:55:04 +0300

    partly made auth

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import './App.css'
+import {BrowserRouter, Route, RouterProvider, Routes} from "react-router-dom";
+import Login from "@/pages/login";
 
 const App = () => (
       // eslint-disable-next-line react/jsx-filename-extension
-    <>
-         test
-    </>
+    <BrowserRouter>
+        <div className="App">
+            <Routes>
+                <Route path="/login" element={ <Login/> } />
+            </Routes>
+        </div>
+    </BrowserRouter>
   )
 
 export default App
