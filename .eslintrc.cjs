05f9830dd3e3952daf622f4918b8cbc0feca8b35
LyubomyrKl <lyubomyrklymyuk2001@gmail.com>
2023-07-07 17:10:08 +0300

    partly implement eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,38 @@
-/* eslint-env node */
-
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
-    'eslint:recommended',
+    "eslint:recommended",
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react-hooks/recommended',
+    "plugin:react/recommended",
+    "airbnb",
+    "airbnb/hooks",
+    "prettier",
   ],
-  parser: '@typescript-eslint/parser',
+  overrides: [],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: true,
-    tsconfigRootDir: __dirname,
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./jsconfig.json",
   },
-  plugins: ['react-refresh'],
+  plugins: ["react", '@typescript-eslint'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "import/no-extraneous-dependencies": [
+      "error",
+      {"devDependencies": true}
     ],
-    '@typescript-eslint/no-non-null-assertion': 'off',
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", 'tsx', 'ts'] }],
+    "import/prefer-default-export": "off", // There can be .js files that export generic stuff (like constants) and sometimes they can have only one export
+    "react/function-component-definition": [
+      "error",
+      {
+        // The copied components from the main app used arrow functions
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
   },
-}
+};
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css'
+
+interface IAppProps{}
+
+const App = () => (
+      // eslint-disable-next-line react/jsx-filename-extension
+    <>
+         test
+    </>
+  )
+
+export default App
